8f0f8805da2b83e06778c8c330b9112db7e52473
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2022-04-02 22:32:13 +0300

    FinalExam

--- ExamPreparation/06 Java DB Advanced Exam - 28 Jul 2019/src/main/java/softuni/exam/repository/PlayerRepository.java
+++ ExamPreparation/06 Java DB Advanced Exam - 28 Jul 2019/src/main/java/softuni/exam/repository/PlayerRepository.java
@@ -1,6 +1,8 @@
 package softuni.exam.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import softuni.exam.domain.entities.Player;
 
@@ -10,6 +12,6 @@ import java.util.List;
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, Integer> {
 
-    List<Player> findAllByTeamNameOrderByIdAsc(String name);
+    List<Player> findAllByTeamNameOrderByIdAsc(@Param("name")String name);
     List<Player> findAllBySalaryGreaterThanOrderBySalaryDesc(BigDecimal salary);
 }
